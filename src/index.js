4a92086f982eb5107b25259029ac6383e1757321
Kieran Lewis <kieranlewis1334@gmail.com>
2021-10-03 11:40:08 +1300

    Button now created a new empty project

--- src/index.js
+++ src/index.js
@@ -1,1 +1,14 @@
 import './style.css';
+import Task from './Task';
+import Project from './Project';
+
+const button = document.querySelector('#submit-btn');
+
+const projects = [];
+
+button.addEventListener('click', (e) => {
+    e.preventDefault();
+    let projectName = document.querySelector('#projectName').value;
+    const newProject = Project(projectName, []);
+    projects.push(newProject);
+});
