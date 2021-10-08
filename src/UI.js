68ed1ab284583c97256ae0b3eba748f935b7d6c3
Kieran Lewis <kieranlewis1334@gmail.com>
2021-10-09 09:35:01 +1300

    created project now goes in sidebar

--- src/UI.js
+++ src/UI.js
@@ -1,5 +1,6 @@
 const projectForm = document.querySelector('#form-container');
 const projectDiv = document.querySelector('#project-container');
+const sidebar = document.querySelector('.w3-sidebar');
 
 export const openForm = () => {
     projectForm.style.display = 'block';
@@ -9,13 +10,9 @@ export const closeForm = () => {
     projectForm.style.display = 'none';
 }
 
-export const displayProjects = (projects) => {
-    projectDiv.innerHTML = '';
-
-    projects.forEach(project => {
-        console.log(project);
-        const header = document.createElement('h2');
-        header.textContent = project.title;
-        projectDiv.appendChild(header);
-    });
-};
+export const displayProjects = (project) => {
+    const header = document.createElement('a');
+    header.textContent = project.title;
+    header.classList.add("w3-bar-item", "w3-button");
+    sidebar.appendChild(header);
+}
